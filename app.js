88259903a418c7ab913c3294fc7f6ee81abfc912
Kate Smychok <e.smychok@yandex.ru>
2021-05-15 23:17:12 +0300

    routes, controllers, models

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+
+const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '609e83f92751cf43f01588b2',
+  };
+  next();
+});
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
@@ -21,4 +28,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT);
-
