f3894f8b04518e23bc76d82bb8437fe981b443fc
KateSmychok <e.smychok@yandex.ru>
2021-06-27 17:31:15 +0300

    add cors module

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-err');
 
 const corsOptions = {
-  origin: ['http://even-star.students.nomoredomains.monster'],
+  origin: ['https://even-star.students.nomoredomains.monster'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Methods', 'Access-Control-Request-Headers'],
   credentials: true,
   enablePreflight: true,
