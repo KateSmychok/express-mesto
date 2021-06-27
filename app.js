9ef0735d693784e59f5410059d92c38a22082b97
KateSmychok <e.smychok@yandex.ru>
2021-06-27 17:04:07 +0300

    add cors module

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/error-handler');
 const { validateEmailAndPassword } = require('./middlewares/validators');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
 const NotFoundError = require('./errors/not-found-err');
+
+const corsOptions = {
+  origin: ['http://even-star.students.nomoredomains.monster'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Methods', 'Access-Control-Request-Headers'],
+  credentials: true,
+  enablePreflight: true,
+};
 
 const app = express();
 
@@ -30,10 +36,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-app.use(cors());
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
-app.post('/signin', validateEmailAndPassword, login);
-app.post('/signup', validateEmailAndPassword, createUser);
+app.post('/sign-in', validateEmailAndPassword, login);
+app.post('/sign-up', validateEmailAndPassword, createUser);
 
 app.use('/users', auth, require('./routes/users'));
 app.use('/cards', auth, require('./routes/cards'));
