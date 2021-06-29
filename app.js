c8a0b8d8afb66ab0430d1f6e563bbbf42aecc06f
KateSmychok <e.smychok@yandex.ru>
2021-06-29 20:41:03 +0300

    add cors middleware

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(requestLogger);
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://even-star.students.nomoredomains.monster');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
 
   next();
 });
